6688f4681c14ab17339db2c5eb5b127a9b298c9a
Shaul Hameed <shaul21h@gmail.com>
2016-04-27 19:18:15 +0530

    #ZTE-7 Completed product page for Verizon

--- public/js/zte-products.js
+++ public/js/zte-products.js
@@ -1,9 +1,37 @@
 $(document).ready(onDocumentReady)
 
+
 function onDocumentReady(){
 
+  var menuRegex = /(about|dimensions|support|specs)/gi
+  $("#menu-section ul li a").bind("click", function(e){
+    var contentList = [".about", ".dimensions", ".specs", ".support"]
+    $("#menu-section").find(".selected").removeClass("selected");
+    $(e.currentTarget.parentElement).addClass("selected", 10000, "swing")
+    var contentId = $(e.target).html().toLowerCase()
+    var hideEm = removeFromArray(contentList, "."+contentId).join(",");
+    $(hideEm).css("display","none")
+    $("#"+contentId).css("display","block")
 
+  })
 
+}
+
+function removeFromArray(array, item) {
+    var itemIndex;
+
+    // Look for the item (the item can have multiple indices)
+    itemIndex = array.indexOf(item);
+
+    while (itemIndex !== -1) {
+        // Remove the item, then return the modified array
+        array.splice(itemIndex, 1);
+
+        itemIndex = array.indexOf(item);
+    }
+
+    // Return the modified array
+    return array;
 }
 
 // Callback for Google Map
